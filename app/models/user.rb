800337b9305cc24c18de54ffede80f46caf5a0fb
arsalan-menhaj <arsalan.menhaj@gmail.com>
2017-10-16 19:20:44 +0000

    Wrote Rspec tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials (email, password)
+    # Removes whitespace from email, and turn letters lowercase
+    email.gsub!(/\s+/, '')
+    email.downcase
+
+    if User.find_by(email: email)
+      User.find_by(email: email).authenticate(password)
+    else
+      return false
+    end
+  end
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,93 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it "creates new User with valid attributes" do
+      User.create!({
+        first_name: 'Bruce',
+        last_name: 'Wayne',
+        email: 'notbatman@batman.com',
+        password: '12345678',
+        password_confirmation: '12345678'
+        }).should be_valid
+    end
+    it "fails to create new User without a password" do
+      User.create({
+        first_name: 'Bruce',
+        last_name: 'Wayne',
+        email: 'notbatman@batman.com',
+        password_confirmation: '12345678'
+        }).should_not be_valid
+    end
+    it "fails to create new User when password and confirmation don't match" do
+      User.create({
+        first_name: 'Bruce',
+        last_name: 'Wayne',
+        email: 'notbatman@batman.com',
+        password: '87654321',
+        password_confirmation: '12345678'
+        }).should_not be_valid
+    end
+    it "fails to create new User without password confirmation" do
+      User.create({
+        first_name: 'Bruce',
+        last_name: 'Wayne',
+        email: 'notbatman@batman.com',
+        password: '12345678'
+        }).should_not be_valid
+    end
+    it "fails to create new User when password is not long enough" do
+      User.create({
+        first_name: 'Bruce',
+        last_name: 'Wayne',
+        email: 'notbatman@batman.com',
+        password: '12345',
+        password_confirmation: '12345'
+        }).should_not be_valid
+    end
+    it "fails to create new User when email is already in use (NOT case-sensitive)" do
+      User.create!({
+        first_name: 'Bruce',
+        last_name: 'Wayne',
+        email: 'notbatman@batman.com',
+        password: '12345678',
+        password_confirmation: '12345678'
+      })
+      User.create({
+        first_name: 'Bruce',
+        last_name: 'Wayne',
+        email: 'NotBatman@batman.com',
+        password: '12345678',
+        password_confirmation: '12345678'
+      }).should_not be_valid
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    before(:each) do
+      @user = User.create!({
+        first_name: 'Hal ',
+        last_name: 'Jordan',
+        email: 'green@lantern.com',
+        password: '12345678',
+        password_confirmation: '12345678'
+      })
+    end
+
+    it "authenticates registered user with email and password" do
+      expect(User.authenticate_with_credentials('green@lantern.com', '12345678')).to eq(@user)
+    end
+
+    it "authenticates registered user even with spaces before the email" do
+      expect(User.authenticate_with_credentials('  green@lantern.com', '12345678')).to eq(@user)
+    end
+
+    it "fails authentication with wrong password" do
+      User.authenticate_with_credentials('green@lantern.com', '12345999').should be false
+    end
+
+    it "fails authentication with non-existent user info" do
+      User.authenticate_with_credentials('barry@allen.com', '12345999').should be false
+    end
+  end
+end

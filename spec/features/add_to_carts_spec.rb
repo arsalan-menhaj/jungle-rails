79f5fe298eef6e0951bc602586da37d0629ffc78
arsalan-menhaj <arsalan.menhaj@gmail.com>
2017-10-17 17:27:00 +0000

    Added rspec test for Add to Cart button

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature do
+  #SETUP
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They click on the Add button for the first product" do
+    visit root_path
+    first("article.product").click_on "Add"
+
+    expect(page.find("ul.navbar-right")).to have_content "My Cart (1)"
+  end
+end
